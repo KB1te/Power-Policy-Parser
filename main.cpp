ab6ce9f6f4494f547ad325cf7acf20aa5ff17f6f
LC-14T <75213117+LC-14T@users.noreply.github.com>
2020-11-29 12:20:27 -0300

    Update main.cpp
    - FIX SWITCH
    - ISO C++
    - CODE QUALITY

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include <Windows.h>
+#include <Windows.h>
 #include <powrprof.h>
 #include <iostream>
 #include <string>
@@ -7,34 +7,43 @@
 #pragma comment(lib,"Powrprof.lib")
 
 typedef struct _SYSTEM_POWER_INFORMATION {
-	ULONG MaxIdlenessAllowed;
-	ULONG Idleness;
-	LONG TimeRemaining;
-	UCHAR CoolingMode;
+    ULONG MaxIdlenessAllowed;
+    ULONG Idleness;
+    LONG TimeRemaining;
+    UCHAR CoolingMode;
 } SYSTEM_POWER_INFORMATION, *PSYSTEM_POWER_INFORMATION;
 
+using namespace std;
+
 int main() {
-	SYSTEM_POWER_INFORMATION pwInfo;
-	CallNtPowerInformation(SystemPowerInformation, NULL, NULL, &pwInfo, sizeof(pwInfo));
+    SYSTEM_POWER_INFORMATION pwInfo;
+    CallNtPowerInformation(SystemPowerInformation, NULL, NULL, &pwInfo, sizeof(pwInfo));
 
-	printf("-----------------------------\n   Power Policy parser   \n-----------------------------\n\n");
+    cout << "-----------------------------\n   Power Policy parser   \n-----------------------------\n\n";
 
-	switch (pwInfo.CoolingMode) {
-	case 0:
-		printf("[+] Cooling mode active\n");
-		break;
-	case 2:
-		printf("[-] The system does not support CPU throttling, or there is no thermal zone defined in the system.\n");
-	case 1:
-		printf("[*] The system is currently in Passive cooling mode.\n");
-	}
-	POWER_POLICY pwPolicy;
-	GLOBAL_POWER_POLICY pwGlPolicy;
+    switch (pwInfo.CoolingMode) {
+    case 0:
+        cout << "[+] Cooling mode active" << endl;
+        break;
+    case 2:
+        cout << "[-] The system does not support CPU throttling, or there is no thermal zone defined in the system." << endl;
+        break;
+    case 1:
+        cout << "[*] The system is currently in Passive cooling mode." << endl;
+        break;
+    
+    default:
+        cout << "Not Expected" << endl;
+        break;
+    }
+    
+    POWER_POLICY pwPolicy;
+    GLOBAL_POWER_POLICY pwGlPolicy;
 
-	GetCurrentPowerPolicies(&pwGlPolicy, &pwPolicy);
+    GetCurrentPowerPolicies(&pwGlPolicy, &pwPolicy);
 
-	printf("[*] The video turn off after -> %lu seconds or %lu minutes\n",pwPolicy.user.VideoTimeoutAc,(pwPolicy.user.VideoTimeoutAc)/60);
-	printf("[*] Time before power to fixed disk drives is turned off -> %lu seconds or %lu minutes\n", pwPolicy.user.SpindownTimeoutAc,(pwPolicy.user.SpindownTimeoutAc)/60);
-	printf("[*] Fan throttle tolerance -> %uc", pwPolicy.user.FanThrottleToleranceAc);
+    printf("[*] The video turn off after -> %lu seconds or %lu minutes\n",pwPolicy.user.VideoTimeoutAc,(pwPolicy.user.VideoTimeoutAc)/60);
+    printf("[*] Time before power to fixed disk drives is turned off -> %lu seconds or %lu minutes\n", pwPolicy.user.SpindownTimeoutAc,(pwPolicy.user.SpindownTimeoutAc)/60);
+    printf("[*] Fan throttle tolerance -> %uc", pwPolicy.user.FanThrottleToleranceAc);
 
 }
